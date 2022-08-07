c941682645348e2f50d0142d0f9c243b2bd011f8
Tshepo Makhubela <tshepo@synthesis.co.za>
2022-08-07 16:34:03 +0200

    Initial commit

---
+++ sqs-consumer.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+    try {
+
+        // Loop through messages from sqs
+        for (const record of event.Records) {
+
+            const messageBody = JSON.parse(record.body);
+
+            let payload = null;
+
+            if (messageBody.isUploadedToS3) {
+
+                const bucketName = 'my-file-bucket-011' // TODO have this as a description
+
+                // Read the payload from s3
+                payload = await getS3Object(bucketName, messageBody.fileKey);
+
+                // Delete the payload file from s3.
+                // Alternatively you can use S3 lifecycle to delete objects from the bucket
+                await deleteS3Object(bucketName, messageBody.fileKey);
+
+            } else {
+
+                // Get the message payload from the SQS event if the message was not sent through S3
+                payload = messageBody.body;
+            }
+
+            console.log('Payload:', JSON.stringify(payload, null, 2));
+
+            // Process the payload here
+        }
+
+    } catch (error) {
+
+        console.log('Error occured', JSON.stringify(error, null, 2));
+
+        return {
+            statusCode: error.statusCode ? error.statusCode : 500,
+            body: JSON.stringify({
+                error: error.name ? error.name : "Exception",
+                message: error.message ? error.message : "Unknown error"
+            })
+        };
+
+    }
+}
+
+
+const getS3Object = async (bucketName, fileKey) => {
+    try {
+
+        const params = {
+            Bucket: bucketName,
+            Key: fileKey
+        }
+
+        const data = await s3.getObject(params).promise();
+
+        return JSON.parse(data.Body.toString('utf-8'));
+
+    } catch (error) {
+
+        console.log('Error getting object', JSON.stringify(error, null, 2));
+
+        throw error;
+
+    }
+
+
+}
+
+const deleteS3Object = async (bucketName, fileKey) => {
+    try {
+        const params = {
+            Bucket: bucketName,
+            Key: fileKey
+        }
+
+        await s3.deleteObject(params).promise();
+
+    } catch (error) {
+
+        console.log('Error deleting object', JSON.stringify(error, null, 2));
+
+        throw error;
+
+    }
+
+}
---
+++ sqs-producer.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+
+const AWS = require('aws-sdk');
+
+const sqs = new AWS.SQS();
+
+const s3 = new AWS.S3();
+
+
+exports.handler = async (event) => {
+  try {
+    
+    // SQS memory limit in bytes
+    const sqsMemoryLimit = 256000;
+    
+    // Get payload from event
+    const payload = event.body;
+
+    // Get size of payload in bytes
+    const payloadMemorySize = getObjectSizeInBytes(payload);
+
+    let queuePayload = {
+      isUploadedToS3: false,
+      fileKey: null,
+      body: null
+    }
+
+    const queueUrl = 'YOUR QUEUE URL'; // TODO replace with queue url
+
+    if (payloadMemorySize >= sqsMemoryLimit) {
+      const fileKey = uuidv4() + '.json';
+
+      const bucketName = 'YOUR BUCKET NAME'; //TODO replace with your bucket name
+
+      await uploadToS3(payload, bucketName, fileKey);
+
+      queuePayload.isUploadedToS3 = true;
+
+      queuePayload.fileKey = fileKey;
+
+    } else {
+
+      queuePayload.body = payload;
+
+    }
+
+    await sendSqsMessage(queuePayload, queueUrl);
+
+  } catch (error) {
+
+    console.log('Error occured', JSON.stringify(error, null, 2));
+
+    return {
+      statusCode: error.statusCode ? error.statusCode : 500,
+      body: JSON.stringify({
+        error: error.name ? error.name : "Exception",
+        message: error.message ? error.message : "Unknown error"
+      })
+    };
+
+  }
+
+};
+
+const getObjectSizeInBytes = (obj) => {
+
+  let str = null;
+
+  if (typeof obj === 'string') {
+
+    str = obj;
+
+  } else {
+
+    str = JSON.stringify(obj);
+
+  }
+
+  // Get the length of the Uint8Array
+
+  const bytes = new TextEncoder().encode(str).length;
+
+  return bytes;
+
+}
+
+const uploadToS3 = async (payload, bucketName, fileKey) => {
+  try {
+
+    const params = {
+      Body: Buffer.from(JSON.stringify(payload)),
+      Bucket: bucketName,
+      Key: fileKey,
+      ContentType: 'application/json'
+    }
+
+    await s3.putObject(params).promise();
+
+  } catch (error) {
+
+    console.log('Error uploading object to S3', JSON.stringify(error, null, 2));
+
+    throw error;
+
+  }
+}
+
+
+const sendSqsMessage = async (messageBody, queueUrl) => {
+  try {
+
+    const params = {
+      MessageBody: JSON.stringify(messageBody),
+      QueueUrl: queueUrl
+    }
+
+    await sqs.sendMessage(params).promise();
+
+  } catch (error) {
+
+    console.log('Error sending sqs message', JSON.stringify(error, null, 2));
+
+    throw error;
+
+  }
+}
